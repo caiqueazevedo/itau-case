e80e7fe6588a34c1bd8967e4e51b67aec073af6c
Caique Matheus <dev.caique@gmail.com>
2021-06-08 21:53:31 -0300

    Exibindo lista de tarefas do banco

--- todo-list/src/app/components/task-list/task-list.component.ts
+++ todo-list/src/app/components/task-list/task-list.component.ts
@@ -1,5 +1,10 @@
+import { TaskDTO } from './../../api/models/task-dto';
 import { Component, OnInit } from '@angular/core';
 import { faTrash, faPencilAlt } from '@fortawesome/free-solid-svg-icons';
+import { Observable } from 'rxjs';
+import {Router} from '@angular/router';
+import { Task } from 'src/app/api/models';
+import { TaskControllerService } from 'src/app/api/services';
 
 @Component({
   selector: 'task-list',
@@ -9,13 +14,18 @@ import { faTrash, faPencilAlt } from '@fortawesome/free-solid-svg-icons';
 
 export class TaskListComponent implements OnInit {
 
+  taskList: TaskDTO[] = [];
+
   faTrash = faTrash;
   faPencilAlt = faPencilAlt;
-  constructor() { 
+
+  constructor(private taskService: TaskControllerService) { 
   }
 
   ngOnInit(): void {
+    this.taskService.indexUsingGET()
+      .subscribe(
+        res => this.taskList = res)
   }
 
-  removeTask(){}
 }
